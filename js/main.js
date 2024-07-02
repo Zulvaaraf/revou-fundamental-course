96519666f2eb13ac838545cff47b3c109a809caa
araf <arafzulvaa@gmail.com>
2024-07-03 01:30:49 +0700

    final submission

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@ function showMenuToggle(toggleId, menuId) {
 }
 
 showMenuToggle('navbar-toggle', 'navbar-menu');
+
+const formSubmit = document.getElementById('form');
+
+formSubmit.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const date = document.getElementById('date').value;
+  const gender = document.getElementById('gender').value;
+  const message = document.getElementById('message').value;
+
+  const outputName = document.getElementById('output-name');
+  const outputDate = document.getElementById('output-date');
+  const outputGender = document.getElementById('output-gender');
+  const outputMessage = document.getElementById('output-message');
+
+  outputName.textContent = name;
+  outputDate.textContent = date;
+  outputGender.textContent = gender;
+  outputMessage.textContent = message;
+
+  formSubmit.reset();
+});
